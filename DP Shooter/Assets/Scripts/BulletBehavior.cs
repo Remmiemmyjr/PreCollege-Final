48274b83e758c96fd1f75d13ddc5d3c1a3c21c63
Remmie <emily@isimar.com>
2020-07-15 14:50:19 -0700

    Added a ricochet, cant shoot in negative direction

--- DP Shooter/Assets/Scripts/BulletBehavior.cs
+++ DP Shooter/Assets/Scripts/BulletBehavior.cs
@@ -2,26 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KillBullet : MonoBehaviour
+public class BulletBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int count;
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
 
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Wall")
+        {
+            count++;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(count >= 1)
         {
             Destroy(this.gameObject);
         }
 
     }
+
 }
