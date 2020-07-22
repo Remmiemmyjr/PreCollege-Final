96f1fc5e72b684e2e8f1681987205a6ce724630a
Remmie <emily@isimar.com>
2020-07-22 15:05:33 -0700

    Added post processing and enemy blocks

---
+++ DP Shooter/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public Vector2 acc;
+    private Vector2 vel;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        vel = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        vel += (acc * Time.deltaTime) * 1.5f;
+        rb.velocity += vel * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        {
+            vel *= -1;
+        }
+    }
+}
+
+
