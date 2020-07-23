a3eba59e8869d407b41c6d16b0ad0e5d648b6156
Remmie <emily@isimar.com>
2020-07-22 22:08:33 -0700

    Fixed enemy follow behavior for now

--- DP Shooter/Assets/Scripts/EnemyBehavior.cs
+++ DP Shooter/Assets/Scripts/EnemyBehavior.cs
@@ -5,39 +5,99 @@ using UnityEngine.SceneManagement;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    public Vector2 acc;
-    private Vector2 vel;
     Rigidbody2D rb;
+    Transform Player;
+    public Vector2 acc;
+    private Vector3 savePos;
+    Vector3 toPlayer = new Vector3(0.0f, 0.0f, 0.0f);
+    int maxDistance = 5;
+    float followSpeed = 1.5f;
+    bool following = false;
+    bool canFollow = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        vel = new Vector3();
+        savePos = new Vector3();
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine(Unstuck());
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();
+        if (Vector3.Distance(transform.position, Player.position) <= maxDistance)
+        {
+            following = canFollow;
+        }
+        if (following)
+        {
+            Follow();
+        }
+        else
+        {
+            Movement();
+        }
+
+        //Debug.Log($"{rb.velocity}, {acc}");
+        
+
     }
 
     void Movement()
     {
-        vel += (acc * Time.deltaTime) * 1.5f;
-        rb.velocity += vel * Time.deltaTime;
+        rb.velocity += (acc * Time.deltaTime) * 3f;
+        //rb.velocity += vel * Time.deltaTime;
+    }
+    private void Follow()
+    {
+        toPlayer = Player.position - transform.position;
+        toPlayer = toPlayer.normalized * followSpeed;
+        rb.velocity = toPlayer;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Wall")
-        {
-            vel *= -1;
-        }
         if (collision.gameObject.tag == "Player")
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+        else
+        {
+            following = false;
+            StartCoroutine(FollowCooldown());
+            rb.velocity *= 0;
+            Debug.Log("Collided");
+            acc *= -1;
+        }
     }
+
+    private IEnumerator Unstuck()
+    {
+        while (true)
+        {
+            if (savePos == transform.position)
+            {
+                rb.velocity *= 0;
+                acc *= -1;
+            }
+            savePos = transform.position;
+            yield return new WaitForSeconds(0.75f);
+        }
+    }
+
+    private IEnumerator FollowCooldown()
+    {
+        if(canFollow)
+        {
+            canFollow = false;
+            yield return new WaitForSeconds(3f);
+            canFollow = true;
+        }
+    }
+
+
+        
 }
 
 
